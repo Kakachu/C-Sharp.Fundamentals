48adaa9d46f0d7370fae85a57510e3ffbea4b84a
Kakachu <kauajardim2004@hotmail.com>
2023-05-22 20:10:33 -0300

    Creating a text editor

--- Projects/TextEditor/TextEditor/Program.cs
+++ Projects/TextEditor/TextEditor/Program.cs
@@ -6,7 +6,7 @@ namespace TextEditor
 	{
 		static void Main(string[] args)
 		{
-
+			Menu();
 		}
 
 		static void Menu()
@@ -17,6 +17,73 @@ namespace TextEditor
 			Console.WriteLine("Type 0 for Exit");
 
 			short option = short.Parse(Console.ReadLine());
+
+			switch (option)
+			{
+				case 0: System.Environment.Exit(0);
+					break;
+				case 1: OpenFile();
+					break;
+				case 2: EditFile();
+					break;
+				default: Menu();
+					break;
+			}
+		}
+
+		static void OpenFile()
+		{
+			Console.Clear();
+			Console.WriteLine("What is the path?");
+			string path = Console.ReadLine();
+
+			using (var file = new StreamReader(path))
+			{
+				string text = file.ReadToEnd();
+				Console.WriteLine("--------------------");
+				Console.WriteLine(text);
+			}
+
+			Console.WriteLine("");
+			Console.ReadLine();
+			Console.Clear();
+
+			Menu();
+		}
+
+		static void EditFile()
+		{
+			string text = "";
+
+			Console.Clear();
+			Console.WriteLine("Type your text below (press 'ESC' to exit)");
+			Console.WriteLine("--------------------");
+			do
+			{
+				text += Console.ReadLine();
+				text += Environment.NewLine;
+			}
+			while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+			Save(text);
+		}
+
+		static void Save(string text)
+		{
+			Console.Clear();
+			Console.WriteLine("What is the path?");
+			var path = Console.ReadLine();
+
+			using (var file = new StreamWriter(path))
+			{
+				file.Write(text);
+			}
+
+			Console.WriteLine($"The file {path} has been saved with sucessfully");
+			Console.ReadLine();
+			Console.Clear();
+
+			Menu();
 		}
 	}
 }

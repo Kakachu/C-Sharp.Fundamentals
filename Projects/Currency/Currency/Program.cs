0cfee76ecf1257fd7b73f42a0c32310008530ce8
Kakachu <kauajardim2004@hotmail.com>
2023-06-05 16:58:33 -0300

    Decimal Format

---
+++ Projects/Currency/Currency/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Currency
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			decimal value = 10.25m;
+			Console.WriteLine("Round - Rounds to nearest value " + Math.Round(value));
+			Console.WriteLine("Ceiling - Rounds to upper value " + Math.Ceiling(value));
+			Console.WriteLine("Floor - Rounds to lower value " + Math.Floor(value));
+		}
+	}
+}
